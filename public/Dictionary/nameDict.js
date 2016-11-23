60290b8ecd9e3529a7345396abe4ee66557478e1
lavgen <lavgen@artic.edu>
2016-11-23 12:13:10 -0600

    map file and Country name dictionary

---
+++ public/Dictionary/nameDict.js
@@ -0,0 +1,588 @@
+var nameDict = {
+	"Afghanistan":{
+		rightName: "Afghanistan"	
+	},
+	"Albania":{
+		rightName: "Albania"	
+	},
+	"Algeria":{
+		rightName: "Algeria"	
+	},
+	"Andorra":{
+		rightName: "Andorra"	
+	},
+	"Angola": {
+		rightName: "Angola"},
+
+	"Antigua and Barbuda" :{
+
+		rightName:"Antigua and Barbuda"},
+	"Argentina" : {
+
+		rightName:"Argentina"},
+
+	"Armenia" : {
+		rightName:"Armenia"},
+
+	"Australia" : {
+		rightName:"Australia"},
+
+	"Austria" : {
+		rightName:"Austria"},
+
+	"Azerbaijan" : {
+		rightName:"Azerbaijan"},
+
+	"The Bahamas" : {
+		rightName:"Bahamas"},
+
+	"Bahrain" : {
+		rightName:"Bahrain"},
+
+	"Bangladesh" : {
+		rightName:"Bangladesh"},
+
+	"Barbados" : {
+		rightName:"Barbados"},
+
+	"Belarus" : {
+		rightName:"Belarus"},
+
+	"Belgium" : {
+		rightName:"Belgium"},
+
+	"Belize" : {
+		rightName:"Belize"},
+
+	"Benin" : {
+		rightName:"Benin"},
+
+	"Bhutan" : {
+		rightName:"Bhutan"},
+
+	"Bolivia" : {
+		rightName:"Bolivia"},
+
+	"Bosnia and Herzegovina" : {
+		rightName:"Bosnia and Herzegovina"},
+
+	"Botswana" : {
+		rightName:"Botswana"},
+
+	"Brazil" : {
+		rightName:"Brazil"},
+
+	"Brunei" : {
+		rightName:"Brunei"},
+
+	"Bulgaria" : {
+		rightName:"Bulgaria"},
+
+	"Burkina Faso" : {
+		rightName:"Burkina Faso"},
+
+	"Burundi" : {
+		rightName:"Burundi"},
+
+	"Cape Verde" : {
+		rightName:"Cabo Verde"},
+
+	"Cambodia" : {
+		rightName:"Cambodia"},
+
+	"Cambodia" : {
+		rightName:"Cameroon"},
+
+	"Canada" : {
+		rightName:"Canada"},
+
+	"Central African Republic" : {
+		rightName:"Central African Republic"},
+
+	"Chad" : {
+		rightName:"Chad"},
+
+	"Chile" : { 
+		rightName:"Chile"},
+
+	"China" : {
+		rightName:"China"},
+
+	"Colombia" : {
+		rightName:"Colombia"},
+
+	"Comoros" : {
+		rightName:"Comoros"},
+
+	"Republic of Congo": {
+		rightName:"Congo"},
+
+	"Costa Rica" : {
+		rightName:"Costa Rica"},
+
+	"Croatia" : { 
+		rightName:"Croatia"},
+
+	"Cuba" : {
+		rightName:"Cuba"},
+
+	"Cyprus" : {
+		rightName:"Cyprus"},
+
+	"Czech Republic" : {
+		rightName:"Czech Republic"},
+
+	"Ivory Coast" : {
+		rightName:"Côte d'Ivoire"},
+
+	 "Denmark" : {
+	 	rightName:"Denmark"},
+
+	"Djibouti" : {
+		rightName:"Djibouti"},
+
+	"Dominica" : {
+		rightName:"Dominica"},
+
+	"Dominican Republic" : {
+		rightName:"Dominican Republic"},
+
+	 "Democratic Republic of the Congo" :{
+	 	rightName:"Congo"},
+
+	"Ecuador" : {
+		rightName:"Ecuador"},
+
+	"Egypt" : {
+		rightName:"Egypt"},
+
+	"El Salvador" : {
+		rightName:"El Salvador"},
+
+	"Equatorial Guinea" : {
+		rightName:"Equatorial Guinea"},
+
+	"Eritrea" : {
+		rightName:"Eritrea"},
+
+	"Estonia" : {
+		rightName:"Estonia"},
+
+	"Ethiopia" : {
+		rightName:"Ethiopia"},
+
+	"Fiji" : {
+		rightName:"Fiji"},
+
+	"Finland" : {
+		rightName:"Finland"},
+
+	"France" : {
+		rightName:"France"},
+
+	"Gabon" : {
+		rightName:"Gabon"},
+
+	"Gambia" : {
+		rightName:"Gambia"},
+
+	"Georgia" : {
+		rightName:"Georgia"},
+
+	"Germany" : {
+		rightName:"Germany"},
+
+	"Ghana" : {
+		rightName:"Ghana"},
+
+	"Greece" : {
+		rightName:"Greece"},
+
+	"Grenada" : {
+		rightName:"Grenada"},
+
+	"Guatemala" : {
+		rightName:"Guatemala"},
+
+	"Guinea" : {
+		rightName:"Guinea"},
+
+	"Guinea Bissau" : {
+		rightName:"Guinea-Bissau"},
+
+	"Guyana" : {
+		rightName:"Guyana"},
+
+	"Haiti" : {
+		rightName:"Haiti"},
+
+	"Vatican" : { 
+		rightName:"Holy See"},
+
+	"Honduras" : {
+		rightName:"Honduras"},
+
+	"Hungary" : {
+		rightName:"Hungary"},
+
+	"Iceland" : {
+		rightName:"Iceland"},
+
+	"India" : {
+		rightName:"India"},
+
+	"Indonesia" : {
+		rightName:"Indonesia"},
+
+	"Iran" : {
+		rightName:"Iran"},
+
+	"Iraq" : {
+		rightName:"Iraq"},
+
+	"Ireland" : {
+		rightName:"Ireland"},
+
+	"Israel" : {
+		rightName:"Israel"},
+
+	"Italy" : {
+		rightName:"Italy"},
+
+	 "Jamaica" : {
+	 	rightName:"Jamaica"},
+
+	"Japan" : {
+		rightName:"Japan"},
+
+	"Jordan" : {
+		rightName:"Jordan"},
+
+	"Kazakhstan" : {
+		rightName:"Kazakhstan"},
+
+	"Kenya" : {
+		rightName:"Kenya"},
+
+	"Kiribati" : {
+		rightName:"Kiribati"},
+
+	"Kuwait" : {
+		rightName:"Kuwait"},
+
+	"Kyrgyzstan" : {
+		rightName:"Kyrgyzstan"},
+
+	"Laos" : {
+		rightName:"Laos"},
+
+	"Latvia" :{
+		rightName:"Latvia"},
+
+	"Lebanon" : {
+		rightName:"Lebanon"},
+
+	"Lesotho" : {
+		rightName:"Lesotho"},
+
+	"Liberia" : {
+		rightName:"Liberia"},
+
+	"Libya" : {
+	 	rightName:"Libya"},
+
+	"Liechtenstein" : {
+		rightName:"Liechtenstein"},
+
+	"Lithuania" : {
+		rightName:"Lithuania"},
+
+	"Luxembourg" : {
+		rightName:"Luxembourg"},
+
+	"Madagascar" : {
+		rightName:"Madagascar"},
+
+	"Malawi" : {
+		rightName:"Malawi"},
+
+	"Malaysia" : {
+		rightName:"Malaysia"},
+
+	"Maldives" : {
+		rightName:"Maldives"},
+
+	"Mali" : {
+		rightName:"Mali"},
+	"Malta" : {
+		rightName:"Malta"},
+
+	"Marshall Islands" : {
+		rightName:"Marshall Islands"},
+
+	"Mauritania" : {
+		rightName:"Mauritania"},
+
+	"Mauritius" : {
+		rightName:"Mauritius"},
+
+	"Mexico" : {
+		rightName:"Mexico"},
+
+	"Federated States of Micronesia" : {
+		rightName:"Micronesia"},
+
+	"Moldova" : {
+		rightName:"Moldova"},
+
+	"Monaco" : {
+		rightName:"Monaco"},
+
+	"Mongolia" : {
+		rightName:"Mongolia"},
+
+	"Montenegro" : {
+		rightName:"Montenegro"},
+
+	"Morocco" : {
+		rightName:"Morocco"},
+
+	"Mozambique" : {
+		rightName:"Mozambique"},
+
+	"Myanmar" : {
+		rightName:"Myanmar"},
+
+	"Namibia" : {
+		rightName:"Namibia"},
+
+	"Nauru" : {
+		rightName:"Nauru"},
+
+	"Nepal" : {
+		rightName:"Nepal"},
+
+	"Netherlands" : {
+		rightName:"Netherlands"},
+
+	"New Zealand" : {
+		rightName:"New Zealand"},
+
+	"Nicaragua" : {
+		rightName:"Nicaragua"},
+
+	"Niger" : {
+		rightName:"Niger"},
+
+	"Nigeria" : {
+		rightName:"Nigeria"},
+
+	"North Korea" : {
+		rightName:"North Korea"},
+
+	"Norway" : {
+		rightName:"Norway"},
+
+	"Oman" : {
+		rightName:"Oman"},
+
+	"Pakistan" : {
+		rightName:"Pakistan"},
+
+	"Palau" : {
+		rightName:"Palau"},
+
+	"Panama" : {
+		rightName:"Panama"},
+
+	"Papua New Guinea" : {
+		rightName:"Papua New Guinea"},
+
+	"Paraguay" : {
+		rightName:"Paraguay"},
+
+	"Peru" : {
+		rightName:"Peru"},
+
+	"Philippines" : {
+		rightName:"Philippines"},
+
+	"Poland" : {
+		rightName:"Poland"},
+
+	"Portugal" : {
+		rightName:"Portugal"},
+
+	"Qatar": {
+		rightName:"Qatar"},
+
+	"Romania" : {
+		rightName:"Romania"},
+
+	"Russia" : {
+		rightName:"Russia"},
+
+	"Rwanda" : {
+		rightName:"Rwanda"},
+
+	"Saint Kitts and Nevis" : {
+		rightName:"Saint Kitts and Nevis"},
+
+	"Saint Lucia" : {
+		rightName:"Saint Lucia"},
+
+	"Samoa": {
+		rightName:"Samoa"},
+
+	"San Marino" : {
+		rightName:"San Marino"},
+
+	"Sao Tome and Principe" : {
+		rightName:"Sao Tome and Principe"},
+
+	"Saudi Arabia" : {
+		rightName:"Saudi Arabia"},
+
+	"Senegal" : {
+		rightName:"Senegal"},
+
+	"Republic of Serbia" : {
+		rightName:"Serbia"},
+
+	"Seychelles" : {
+		rightName:"Seychelles"},
+
+	"Sierra Leone" : {
+		rightName:"Sierra Leone"},
+
+	"Singapore" : {
+		rightName:"Singapore"},
+
+	"Slovakia" : {
+		rightName:"Slovakia"},
+
+	"Slovenia" : {
+		rightName:"Slovenia"},
+
+	"Solomon Islands" : {
+		rightName:"Solomon Islands"},
+
+	"Somalia": {
+		rightName:"Somalia"},
+
+	"South Africa" : {
+		rightName:"South Africa"},
+
+	"South Korea" : {
+		rightName:"South Korea"},
+
+	"South Sudan" : {
+		rightName:"South Sudan"},
+
+	"Spain": {
+		rightName:"Spain"},
+
+	"Sri Lanka" : {
+		rightName:"Sri Lanka"},
+
+	"Saint Vincent and the Grenadines" : {
+		rightName:"St. Vincent & Grenadines"},
+
+	"Palestine" : {
+		rightName:"Palestine"},
+
+	"Sudan" : {
+		rightName:"Sudan"},
+
+	"Suriname" : {
+		rightName:"Suriname"},
+
+	"Swaziland" : {
+		rightName:"Swaziland"},
+
+	"Sweden" : {
+		rightName:"Sweden"},
+
+	"Switzerland" : {
+		rightName:"Switzerland"},
+
+	"Syria" : {
+		rightName:"Syria"},
+
+	"Tajikistan" : {
+		rightName:"Tajikistan"},
+
+	"United Republic of Tanzania": {
+		rightName:"Tanzania"},
+
+	"Macedonia" : {
+		rightName:"Macedonia"},
+
+	"Thailand" : {
+		rightName:"Thailand"},
+
+	"East Timor" : {
+		rightName:"Timor-Leste"},
+
+	"Togo" : {
+		rightName:"Togo"},
+
+	"Tonga" : {
+		rightName:"Tonga"},
+
+	"Trinidad and Tobago" : {
+		rightName:"Trinidad and Tobago"},
+
+	"Tunisia" : {
+		rightName:"Tunisia"},
+
+	"Turkey": {
+		rightName:"Turkey"},
+
+	"Turkmenistan" : {
+		rightName:"Turkmenistan"},
+
+	"Tuvalu" : {
+		rightName:"Tuvalu"},
+
+	"United Kingdom" : {
+		rightName:"United Kingdom"},
+
+	"United States of America" : {
+		rightName:"United States"},
+
+	"Uganda" : {
+		rightName:"Uganda"},
+
+	"Ukraine" : {
+		rightName:"Ukraine"},
+
+	"United Arab Emirates" :{
+		rightName:"United Arab Emirates"},
+
+	"Uruguay" : {
+		rightName:"Uruguay"},
+
+	"Uzbekistan" : {
+		rightName:"Uzbekistan"},
+
+	"Vanuatu" : {
+		rightName:"Vanuatu"},
+
+	"Venezuela" : {
+		rightName:"Venezuela"},
+
+	"Vietnam" : {
+		rightName:"Vietnam"},
+
+	"Yemen" : {
+		rightName:"Yemen"},
+
+	"Zambia" : {
+		rightName:"Zambia"},
+
+	"Zimbabwe" : {
+		rightName:"Zimbabwe"}
+}
+
+
